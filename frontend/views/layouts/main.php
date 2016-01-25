52e4e1266d45f11e3c351dff96a92ba4c9c6baa9
strong <strongilsa@163.com>
2016-01-25 14:16:20 +0800

    a

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -46,7 +46,7 @@ AppAsset::register($this);
             <div class="t_le">欢迎光临趣淘学<b>北京<a href="#">[切换城市]</a></b></div>
             <div class="t_ri">
                 <span><a href="<?= Url::to(['user/register']); ?>">注册 |</a> <a href="denglu.html" class="a1">登陆</a></span>
-                <span class="mtx"><a href="<?= Url::to(['user/index']); ?>">我的趣淘学</a></span>
+                <span class="mtx"><a href="<?= Url::to(['consumption/index']); ?>">我的趣淘学</a></span>
                 <span> <a href="#">帮助中心</a></span>
                 <span class="app"><a href="#">APP</a></span>
             </div>
