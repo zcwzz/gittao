6c08940081485c6fbb0505becf22e1fd4885a1bd
strong <strongilsa@163.com>
2016-01-26 14:43:11 +0800

    asd

--- backend/config/main-local.php
+++ backend/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=127.0.0.1;dbname=final',
+            'dsn' => 'mysql:host=101.200.241.218;dbname=final',
             'username' => 'root',
-            'password' => 'root',
+            'password' => '',
             'charset' => 'utf8',
         ],
         'mailer' => [
